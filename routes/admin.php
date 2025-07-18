2e37c6479dcc6974534cd1c27c4d9da2f5f28cc4
Saif <websiteamz@gmail.com>
2025-07-18 16:03:03 +0400

    Update Routes

--- routes/admin.php
+++ routes/admin.php
@@ -1,14 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\Admin\SlotController;
+use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\CourseController;
 use App\Http\Controllers\Admin\ServiceController;
 use App\Http\Controllers\Admin\CategoryController;
-use App\Http\Controllers\Admin\CreditPackageController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\MembershipController;
-use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\CreditPackageController;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 Route::resource('users', UserController::class);
@@ -19,3 +20,12 @@ Route::resource('slots', SlotController::class);
 Route::get('bookings-list', [SlotController::class, 'bookingsList'])->name('bookings.index');
 Route::resource('memberships', MembershipController::class);
 Route::resource('credits', CreditPackageController::class);
+
+Route::get('/clear-cache', function () {
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('route:clear');
+    Artisan::call('view:clear');
+    Artisan::call('config:cache');
+    return 'Cache cleared and config cached!';
+});
